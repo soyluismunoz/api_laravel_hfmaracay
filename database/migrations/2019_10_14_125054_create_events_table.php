3c94b8935212e691552e2cb6f11181255db67b94
Domingo Oropeza <dioh_@hotmail.com>
2019-10-14 10:11:33 -0300

    feature: agregado el esquelo para el crud de eventos

---
+++ database/migrations/2019_10_14_125054_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->dateTime('date');
+            $table->string('address');
+            $table->string('description')->nullable();
+            $table->string('mapUrl')->nullable();
+            $table->bigInteger('image_id')->nullable()->unsigned();
+            $table->foreign('image_id');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
